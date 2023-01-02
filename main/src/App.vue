b04f06a621c555c6a8bd10700d27e4f499c1b049
Hugo Barcelos <hugo.filipesilvabarcelos@gmail.com>
2023-01-02 21:30:01 -0100

    create vue app for portfolio

---
+++ main/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header/>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
---
+++ main/src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <html lang="en">
+<body>
+    <header>
+        <div class="header">
+            <div class="inner_header">
+                <div class="logo_container">
+                    <h1>MY <span>SITE</span></h1>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+*{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+}
+
+.header {
+    width: 100%;
+    height: 60px;
+    display: block;
+    background-color: rgb(56, 56, 240);
+}
+
+.inner_header{
+    width: 1000px;
+    height: 100%;
+    display: block;
+    margin: 0 auto;
+    background-color: rgb(4, 15, 15);
+}
+
+.logo_container{
+    height: 100%;
+    display: table;
+    float: left;
+}
+
+.logo_container h1{
+    color: white;
+    height: 100%;
+    display: table-cell;
+    
+}
+</style>
