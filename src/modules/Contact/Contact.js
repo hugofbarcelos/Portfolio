016b99317a5affa11e3f16f010b61bcbbb1feaa9
hugofbarcelos <92545069+hugofbarcelos@users.noreply.github.com>
2023-10-08 18:27:16 +0000

    added email.js sending form to my email and some adjutments in app.css

--- src/modules/Contact/Contact.js
+++ src/modules/Contact/Contact.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Contact.css';
 import LinkedinIcon from './icon-linkedin-50.png';
 import GithubIcon from './icon-github-50.png';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+
+
+    e.preventDefault();
+
+
+    emailjs.sendForm('service_gmrizbh', 'template_is98d2n', form.current, 'I-gzM1BKGb-df5OY1')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
   return (
     <div>
       <div className="contactContainer">
         <h1 className="custom-font contactText">Contact Me</h1>
       </div>
       <div>
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
           <label className="labelContainer custom-font">
             First Name
-            <input type="text" name="name" />
+            <input type="text" name="user_name" />
           </label>
           <label className="labelContainer custom-font">
             Last Name
@@ -21,14 +38,14 @@ const Contact = () => {
           </label>
           <label className="labelContainer custom-font">
             Email
-            <input type="text" name="name" />
+            <input type="email" name="user_email" />
           </label>
           <label className="labelContainer custom-font">
             Comments/Questions
-            <textarea type="text" name="name" className='commentsInput' textarea/>
+            <textarea type="text" name="user_message" className='commentsInput' textarea/>
           </label>
           <div className='submitContainer'>
-            <input type="submit" value="Submit" className='submitButton custom-font'/>
+            <input type="submit" value="Send" className='submitButton custom-font'/>
           </div>
         </form>
       </div>
